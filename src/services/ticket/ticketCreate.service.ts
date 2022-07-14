c606bf26291b8363bc9788771c96f9c01827dffd
paulo <paulodgsouza@gmail.com>
2022-07-14 13:23:54 -0300

    fix: create ticket request

--- src/services/ticket/ticketCreate.service.ts
+++ src/services/ticket/ticketCreate.service.ts
@@ -1,43 +1,38 @@
-// import { AppDataSource } from "../../data-source";
-// import { ITicketRequest, ITicket } from "../../interfaces/ticket";
-// import { Ticket } from "../../entities/Tickets";
-
-// const ticketCreateService = async ({ 
-//   userId, zoneId
-// } : ITicketRequest): Promise<ITicket> => {
-
-//   const ticketRepository =  AppDataSource.getRepository(Ticket);
-
-//   // check if zoneId exists
-//   // check if zone has total_tickets > created Tickets
-
-//   const newTicket = ticketRepository.create({
-//     userId,
-//     zoneId,
-//   });
-//   await ticketRepository.save(newTicket);
-
-//   return newTicket;
-// }
-
-// export default ticketCreateService;
 import { ITicketRequest, ITicket } from "../../interfaces/ticket";
 import { AppDataSource } from "../../data-source";
 import { Ticket } from "../../entities/Tickets";
+import { Zone } from "../../entities/Zone";
+import AppError from "../../errors/app-error";
 
 const ticketCreateService = async ({ 
   userId, zoneId
 } : ITicketRequest) =>{
 
   const ticketRepository = AppDataSource.getRepository(Ticket);
+  const zoneRepository = AppDataSource.getRepository(Zone);
 
+  const zone = await zoneRepository.findOneBy({
+    id: zoneId,
+  });
+
+  if(!zone){
+    throw new AppError("Zone not found", 400);
+  }
+  
+  const zones = await zoneRepository.find();
+  if (zones.length > zone.total_tickets){
+    throw new AppError("All tickets from this zone were already created", 409);
+  }
+
+  // using "any" because of Type DeepPartial issue when used with generics #2904
   const newTicket = ticketRepository.create({
     userId,
     zoneId,
-  });
+  } as any);
 
   await ticketRepository.save(newTicket);
-  return newTicket;
+
+  return {...newTicket, userId, zoneId};
 }
 
 export default ticketCreateService;
