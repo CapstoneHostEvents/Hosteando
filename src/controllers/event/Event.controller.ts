a44c52d2d956d88a0ff200b76b8762c32aa2214d
Jaqueline <jaqueline.salturi@hotmail.com>
2022-07-14 16:11:40 -0300

    fix: add userId to DB

--- src/controllers/event/Event.controller.ts
+++ src/controllers/event/Event.controller.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express"
-import { CreateEventService } from "../services/createEvent.service"
-import { ListEventService } from "../services/listEvents.service"
+import { CreateEventService } from "../../services/event/createEvent.service"
+import { ListEventService } from "../../services/event/listEvents.service"
 
 export class CreateEventController {
   static async create (req: Request, res: Response) {
     const { name, description, date } = req.newEvent
+    const user = req.user
     
-    const newEvent = await CreateEventService({name, description, date})
+    const newEvent = await CreateEventService({name, description, date, user})
 
     return res.json(newEvent)
   }
--- src/interfaces/events/index.ts
+++ src/interfaces/events/index.ts
@@ -2,4 +2,11 @@ export interface IEventRequest {
   name: string
   description: string
   date: Date
+  user?: any
+}
+
+export interface IEventResponse {
+  name: string
+  description: string
+  date: Date
 }
