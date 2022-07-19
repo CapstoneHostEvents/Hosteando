07f645de2d05eeadc238adc291fd28a4e3e8ba6a
Miguel Natan Tomaz <miguelnatantomaz@hotmail.com>
2022-07-19 11:37:28 -0300

    :hammer: refactor data source and .env.example for deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,24 +2,36 @@ import { DataSource } from "typeorm";
 import { config } from "dotenv";
 config();
 
-export const AppDataSource = 
-process.env.NODE_ENV === "test"
-  ? new DataSource({
-    type: "sqlite",
-    database: ":memory:",
-    entities: ["src/entities/**/*.ts"],
-    synchronize: true,
-  }) :
-
-  new DataSource({
-  type: "postgres",
-  host: process.env.DB_HOST,
-  port: 5432,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB,
-  synchronize: false,
-  // logging: true,
-  entities: ["src/entities/*.ts"],
-  migrations: ["src/migrations/*.ts"],
-});
+export const AppDataSource =
+  process.env.NODE_ENV === "test"
+    ? new DataSource({
+        type: "sqlite",
+        database: ":memory:",
+        entities: ["src/entities/*.ts"],
+        synchronize: true,
+      })
+    : process.env.NODE_ENV === "production"
+    ? new DataSource({
+        type: "postgres",
+        url: process.env.DATABASE_URL,
+        ssl:
+          process.env.NODE_ENV === "production"
+            ? { rejectUnauthorized: false }
+            : false,
+        synchronize: false,
+        logging: true,
+        entities: ["dist/src/entities/*.js"],
+        migrations: ["dist/src/migrations/*.js"],
+      })
+    : new DataSource({
+        type: "postgres",
+        host: process.env.DB_HOST,
+        port: 5432,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB,
+        synchronize: false,
+        logging: true,
+        entities: ["src/entities/*.ts"],
+        migrations: ["src/migrations/*.ts"],
+      });
