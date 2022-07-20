c911d8229b4bb3145b076a83774cbb9c9403e902
Caio Giffoni <caio_cgf@hotmail.com>
2022-07-20 12:02:23 -0300

    :bug: removed password return from events and zones

--- src/interfaces/zone/index.ts
+++ src/interfaces/zone/index.ts
@@ -1,3 +1,6 @@
+import { Ticket } from "../../entities/Tickets";
+import { IEventList } from "../event";
+
 export interface IZoneRequest {
   name: string;
   price: number;
@@ -15,4 +18,13 @@ export interface IZoneCreate {
 
 export interface IZoneUpdate extends IZoneRequest {
   zoneId: string;
+}
+
+export interface IZoneList {
+  total_tickets: number;
+  id: string;
+  name: string;
+  price: number;
+  created_at: Date;
+  event: IEventList;
 }
--- src/services/zone/zoneList.services.ts
+++ src/services/zone/zoneList.services.ts
@@ -1,15 +1,36 @@
 import { AppDataSource } from "../../data-source";
 import { Zone } from "../../entities/Zone";
 import AppError from "../../errors/app-error";
+import { IZoneList } from "../../interfaces/zone";
 
-const listZoneService = async (): Promise<Zone[]> => {
+const listZoneService = async (): Promise<IZoneList[]> => {
   const zoneRepository = AppDataSource.getRepository(Zone);
 
   const zones = await zoneRepository.find();
 
   if (zones.length === 0) throw new AppError("No zone found", 404);
 
-  return zones;
+  const zoneReturned: IZoneList[] = [];
+
+  zones.map((zone) => {
+    zoneReturned.push({
+      ...zone,
+      event: {
+        created_at: zone.event.created_at,
+        id: zone.event.id,
+        date: zone.event.date,
+        description: zone.event.description,
+        name: zone.event.name,
+        user: {
+          id: zone.event.user.id,
+          name: zone.event.user.name,
+          email: zone.event.user.email,
+        },
+      },
+    });
+  });
+
+  return zoneReturned;
 };
 
 export default listZoneService;
